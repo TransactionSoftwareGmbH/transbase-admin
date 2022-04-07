c12d46a1b379e0845a559cf1cba8ac72d3239a21
dloibl <daniel.loibl@gmail.com>
2022-04-07 18:05:24 +0200

    introduce codemirror as rich sql editor

--- client/src/Resource/SqlQuery.tsx
+++ client/src/Resource/SqlQuery.tsx
@@ -2,31 +2,40 @@ import React from "react";
 import { Button, useDataProvider, List } from "react-admin";
 import { TransbaseDataProvider } from "../provider/api";
 import { ResultSet } from "./ResultSet";
+import { EditorState, EditorView, basicSetup } from "@codemirror/basic-setup";
+import { sql as sqlLang } from "@codemirror/lang-sql";
 
 export function SqlQuery(props) {
   const provider = useDataProvider<TransbaseDataProvider>();
-
-  const [query, setQuery] = React.useState("");
-  const [sql, setSql] = React.useState("");
+  const ref = React.useRef<HTMLDivElement>();
+  const [sqlQuery, setSqlQuery] = React.useState("");
   const [schema, setSchema] = React.useState();
+  const editor = React.useMemo(() => {
+    return new EditorView({
+      state: EditorState.create({
+        extensions: [basicSetup, sqlLang()],
+      }),
+      parent: ref.current!,
+    });
+  }, [ref.current]);
+
   async function execute() {
-    setSql(query);
+    const query = editor.state.doc.toString();
+    setSqlQuery(query);
     const result = await provider.executeQuery(query);
     setSchema(result.schema);
   }
 
   return (
     <>
-      <textarea
-        rows={10}
-        value={query}
-        onChange={(e) => {
-          setQuery(e.target.value);
-        }}
+      <div id="editor" ref={ref} />
+      <Button
+        // disabled={!editor.state.doc.length}
+        label="Run"
+        onClick={execute}
       />
-      <Button disabled={!query} label="Run" onClick={execute} />
-      {sql && schema && (
-        <QueryResultTable {...props} sql={sql} schema={schema} />
+      {schema && sqlQuery && (
+        <QueryResultTable {...props} sql={sqlQuery} schema={schema} />
       )}
     </>
   );
